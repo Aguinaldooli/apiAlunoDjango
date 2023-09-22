5e11eb7f7d4296b551d824b32bdf90df472b32af
Aguinaldo Mendes de Oliveira junior <123522287+Aguinaldooli@users.noreply.github.com>
2023-09-22 20:43:09 -0300

    style: add comments to code in alunoList

--- api/views/alunoView/alunoList.py
+++ api/views/alunoView/alunoList.py
@@ -7,9 +7,12 @@ from api.serializers.alunoSerializer import AlunoSerializer
 class AlunoList(APIView):
     def get(self, request):
         try:
+            # Tenta buscar todos os objetos AlunoModel no banco de dados
             alunos = AlunoModel.objects.all()
             serializer = AlunoSerializer(alunos, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
+            # Se ocorrer uma exceção (erro), retorna uma resposta HTTP 500 (Internal Server Error)
+            # com uma mensagem indicando que o aluno não foi criado e a descrição do erro
             return Response({"Não foi criado o aluno!": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
