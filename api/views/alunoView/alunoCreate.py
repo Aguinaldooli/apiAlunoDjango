4cd17827f63cf9f6261dc3eb5ffafbd551463908
Aguinaldo Mendes de Oliveira junior <123522287+Aguinaldooli@users.noreply.github.com>
2023-09-22 20:36:17 -0300

    style: add comments to code in alunoCreate

--- api/views/alunoView/alunoCreate.py
+++ api/views/alunoView/alunoCreate.py
@@ -7,8 +7,11 @@ from api.serializers.alunoSerializer import AlunoSerializer
 class AlunoCreate(APIView):
     def post(self, request):
         serializer = AlunoSerializer(data=request.data)
-
+        # Verifica se os dados fornecidos são válidos de acordo com o serializador          
         if serializer.is_valid():
+            # Se for válido ele vai salvar e retornar uma menssagem com êxito
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            message = "Aluno criado com êxito."
+            return Response({"message": message, "data": serializer.data}, status=status.HTTP_201_CREATED)
+        # Caso não seja válido os dados do body ele retorna o erro específico
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
